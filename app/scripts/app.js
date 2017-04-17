615b11da4a504366e8935f219515489be3e520f9
Jake <jdai54@gmail.com>
2017-04-17 15:50:05 -0700

    Checkpoint 1 Bloc-Chat: Complete

---
+++ app/scripts/app.js
@@ -0,0 +1,26 @@
+// the root Angular module will act as a container for different parts of our application. The first argument passed, blocChat, is the prescribed name of the module. The empty array, passed as the second arugment injexts dependencies into an application. 
+(function() {
+  // use $stateProvider to configure the state behavior and use $locationProvider to configure how the application handles URLs in the browser
+  function config($stateProvider, $locationProvider) {
+    // locationProvider configures an applications paths
+    $locationProvider
+      .html5Mode({
+      // disables hashbang (#!) URLs
+        enabled: true,
+      // remove requirement for specifying the base URL for the application after using html5Mode
+        requireBase: false
+      });
+    // stateProvider, a component of UI-Router, will determine a number of properties for a state. For Bloc Jams, we need to configure at least four aspects of state: its name, URL route, controller, and template. stateProvider calls .state() which takes two arguments: stateName and stateConfig
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        controller: 'LandingCtrl as landing',
+        templateUrl: '/templates/landing.html'
+      });
+    
+  }
+  
+  angular
+    .module('blocChat', ['ui.router', 'firebase'])
+    .config(config);
+})();

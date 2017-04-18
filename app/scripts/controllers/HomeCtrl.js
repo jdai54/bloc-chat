147c6bf4e77037b5543ac20d5fdf8a2522ad2554
Jake <jdai54@gmail.com>
2017-04-18 12:49:54 -0700

    user-story-2-create-chat-rooms: in progress

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,12 @@
+// Inject the Room service by passing it as the paramenter and as a dependency //
 (function() {
   function HomeCtrl(Room) {
+    // assign the array of objects retrieved by the all method to a $scope variable using this.rooms //
     this.rooms = Room.all;
   }
   
   angular
     .module('blocChat')
+    //The last item in the array must be the callback function that executes when the controller is initialized – in this case, HomeCtrl //
     .controller('HomeCtrl', ['Room', HomeCtrl]);
 })();

3e6622420c2b772e1dd2ef9dd120a19e48bf1348
Jake <jdai54@gmail.com>
2017-04-21 14:21:10 -0700

    user-story-2-create-chat-rooms: modal alternative

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,11 +3,12 @@
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
     
-    Room.addRoom = function() {
+    var addRoom = function() {
       rooms.$add({ room: "Room 4" });
     };
     return {
-      all: rooms
+      all: rooms,
+      addRoom: addRoom
     };
   }
     

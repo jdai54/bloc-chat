76236492454d214d17f2e2fbb6f8715e28cfee0d
Jake <jdai54@gmail.com>
2017-04-26 10:41:15 -0700

    user-story-3-list-messages: injector issue

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,6 @@
 // Inject the Room service by passing it as the paramenter and as a dependency //
 (function() {
-  function HomeCtrl(Room) {
+  function HomeCtrl(Room, Message) {
     // assign the array of objects retrieved by the all method to a $scope variable using this.rooms //
     this.showme = false;
     this.rooms = Room.all;
@@ -8,11 +8,15 @@
       Room.addRoom(roomName);
       this.showme = false;
     }
+    this.select = function(roomName) {
+      this.activeRoom = roomName;
+      this.messages = Message.getByRoomId(this.activeRoom.$roomId);
+    }
 
   }
   
   angular
     .module('blocChat')
     //The last item in the array must be the callback function that executes when the controller is initialized – in this case, HomeCtrl //
-    .controller('HomeCtrl', ['Room', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message', HomeCtrl]);
 })();
